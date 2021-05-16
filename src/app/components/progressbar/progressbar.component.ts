065fa92cb6ad9b605f42c1c010669513767fc544
Atilla Altun <atilla.altun@ext.etiya.com>
2021-05-16 04:00:59 +0300

    added progressbar

---
+++ src/app/components/progressbar/progressbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progressbar',
+  templateUrl: './progressbar.component.html',
+  styleUrls: ['./progressbar.component.scss'],
+})
+export class ProgressbarComponent {
+  @Input()
+  value = 0;
+
+  @Input()
+  max = 100;
+}
