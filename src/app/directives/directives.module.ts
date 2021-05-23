7f03f156e55fa8f8d56ad116db11165fe0f9d358
devatil <ngregister@outlook.com>
2021-05-23 15:10:59 +0300

    debounce-click

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DirectiveDocumentationComponent } from './directive-documentation/directive-documentation.component';
 import { RouterModule } from '@angular/router';
 import { DRV_ROUTES } from './directives.routing';
+import { ShareModule } from '../share/share.module';
+import { DebounceClickDirective } from './debounce-click/debounce-click.directive';
 
 @NgModule({
-  declarations: [DirectiveDocumentationComponent],
-  imports: [CommonModule, RouterModule.forChild(DRV_ROUTES)],
+  declarations: [DirectiveDocumentationComponent, DebounceClickDirective],
+  imports: [CommonModule, ShareModule, RouterModule.forChild(DRV_ROUTES)],
 })
 export class DirectivesModule {}
