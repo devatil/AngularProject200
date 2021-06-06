56bce864fbea2fe55bff4f097e45a0d78b06b4f7
devatil <ngregister@outlook.com>
2021-06-06 15:40:05 +0300

    add ripple directive

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -5,9 +5,14 @@ import { RouterModule } from '@angular/router';
 import { DRV_ROUTES } from './directives.routing';
 import { ShareModule } from '../share/share.module';
 import { DebounceClickDirective } from './debounce-click/debounce-click.directive';
+import { RippleDirective } from './ripple-button/ripple.directive';
 
 @NgModule({
-  declarations: [DirectiveDocumentationComponent, DebounceClickDirective],
+  declarations: [
+    DirectiveDocumentationComponent,
+    DebounceClickDirective,
+    RippleDirective,
+  ],
   imports: [CommonModule, ShareModule, RouterModule.forChild(DRV_ROUTES)],
 })
 export class DirectivesModule {}
---
+++ src/app/directives/ripple-button/ripple.directive.ts
@@ -0,0 +1,21 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appRipple]',
+})
+export class RippleDirective {
+  @Input()
+  appRipple = '';
+
+  constructor(public element: ElementRef, public renderer: Renderer2) {}
+
+  @HostListener('mouseenter') onHover() {
+    this.renderer.addClass(this.element.nativeElement, this.appRipple);
+  }
+}
